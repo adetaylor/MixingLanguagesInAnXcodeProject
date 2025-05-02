d0cd5f2aaaafd14d7ad5b4301b8b9b6d3e22c8a2
Adrian Taylor <adrian_taylor@apple.com>
2025-05-02 10:40:35 +0100

    Another comment

--- Modules/header.h
+++ Modules/header.h
@@ -1,6 +1,10 @@
 // Disabling the "pragma once" and switching to the regular header guards
 // fixes the build.
 
+// These modules files are not represented in the .xcproj so don't show up
+// in the XCode editor. For some reason that makes the reproduction case
+// stop working.
+
 #pragma once
 
 //#ifndef header_h

d3b6678858299cbd99d5136f7dbc0cf5c470922e
Adrian Taylor <adrian_taylor@apple.com>
2025-05-02 10:35:55 +0100

    Add comments explaining the problem

--- Modules/header.h
+++ Modules/header.h
@@ -1,3 +1,12 @@
+// Disabling the "pragma once" and switching to the regular header guards
+// fixes the build.
+
 #pragma once
+
+//#ifndef header_h
+//#define header_h
+
 #define MY_MACRO 5
 typedef int my_int;
+
+//#endif // header_h

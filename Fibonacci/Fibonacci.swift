4ab574c9914b9fd9692dca4bab10b89b71004c37
Adrian Taylor <adrian_taylor@apple.com>
2025-09-12 15:49:52 +0100

    Demonstrate quandary about passing reference count from C++ to Swift.

--- Fibonacci/Fibonacci.swift
+++ Fibonacci/Fibonacci.swift
@@ -23,8 +23,24 @@ public struct FibonacciCalculator {
             return 1.0
         }
         
+        testRefIssues();
+        
         // Create the C++ `FibonacciCalculatorCplusplus` class and invoke its `fibonacci` method.
         let cxxCalculator = FibonacciCalculatorCplusplus(printInvocation)
         return cxxCalculator.fibonacci(value - 1.0) + cxxCalculator.fibonacci(value - 2.0)
+    }
+    
+    func testRefIssues() {
+        let a = getPayloadRef(7); // : WTF.Ref<WTF.Payload, WTF.RawPtrTraits<WTF.Payload>, WTF.DefaultRefDerefTraits<WTF.Payload>>
+        // let v = a.value();         // no such method available in Swift
+        let b = a.ptr();
+        let v = b.value();            // calling method on the Payload class
+        print ("Value is \(v).")
+
+        var a2 = Optional.some(getPayloadRef(8));
+        let b2 = a2!.ptr();
+        a2 = Optional.none;
+        let v2 = b2.value();          // use-after-free
+        print ("Value is \(v2).")
     }
 }
--- Fibonacci/FibonacciCplusplus.hpp
+++ Fibonacci/FibonacciCplusplus.hpp
@@ -7,6 +7,18 @@ The C++ API.
 
 #pragma once
 
+#import <iostream>
+
+
+#define _CXX_INTEROP_STRINGIFY(_x) #_x
+#define SWIFT_SHARED_REFERENCE(_retain, _release)                                \
+  __attribute__((swift_attr("import_reference")))                          \
+  __attribute__((swift_attr(_CXX_INTEROP_STRINGIFY(retain:_retain))))      \
+  __attribute__((swift_attr(_CXX_INTEROP_STRINGIFY(release:_release))))
+
+#define SWIFT_RETURNS_UNRETAINED                                               \
+  __attribute__((swift_attr("returns_unretained")))
+
 class FibonacciCalculatorCplusplus {
 public:
     FibonacciCalculatorCplusplus(bool printInvocation);
@@ -14,3 +26,56 @@ public:
 private:
     bool printInvocation;
 };
+
+template<class T>
+class Ref {
+public:
+    T* ptr() const __attribute__((returns_nonnull)) { return ref; }
+    T* ref;
+};
+
+template<class T>
+class RefCounted {
+public:
+    RefCounted() : refCount(0) {}
+    void ref() { refCount++; std::cout << "Referenced; refCount " << refCount << std::endl; }
+    void deref() { refCount--; std::cout << "Dereferenced; refCount " << refCount << std::endl; if (refCount == 0) { delete this; }}
+    virtual ~RefCounted() {}
+private:
+    int refCount;
+};
+
+template<class T>
+Ref<T> adoptRef(T& thingy) {
+    thingy.ref();
+    Ref<T> ref;
+    ref.ref = &thingy;
+    return ref;
+};
+
+class Payload;
+
+inline void payloadRef(Payload* p);
+inline void payloadDeref(Payload* p);
+
+class Payload final : public RefCounted<Payload> {
+public:
+    static Ref<Payload> create(int value) { return adoptRef(*new Payload(value)); }
+
+    ~Payload() { std::cout << "Destroying Payload with value " << m_value << std::endl; }
+
+    int value() const { return m_value; }
+    void setValue(int value) { m_value = value; }
+
+private:
+    explicit Payload(int value) : m_value(value) { std::cout << "Creating Payload with value " << value << std::endl; }
+    int m_value;
+} SWIFT_SHARED_REFERENCE(payloadRef, payloadDeref);
+
+inline Ref<Payload> getPayloadRef(int value) {
+    return Payload::create(value);
+}
+
+inline void payloadRef(Payload* p) { std::cout << "Swift referencing Payload with value " << p->value() << std::endl; p->ref(); }
+inline void payloadDeref(Payload* p) { std::cout << "Swift dereferencing Payload with value " << p->value() << std::endl;  p->deref(); }
+

3e86370698c33a40cad88b91ae40862ef874dee7
Adrian Taylor <adrian_taylor@apple.com>
2025-09-24 21:25:21 +0100

    Cannot use swift::String in a C++ function we want to call from Swift

--- Fibonacci/Fibonacci.swift
+++ Fibonacci/Fibonacci.swift
@@ -23,6 +23,9 @@ public struct FibonacciCalculator {
             return 1.0
         }
         
+        let some_string = makeSwiftStringFromChar(97);
+        print("[Answer is \(some_string)]");
+
         // Create the C++ `FibonacciCalculatorCplusplus` class and invoke its `fibonacci` method.
         let cxxCalculator = FibonacciCalculatorCplusplus(printInvocation)
         return cxxCalculator.fibonacci(value - 1.0) + cxxCalculator.fibonacci(value - 2.0)
--- Fibonacci/FibonacciCplusplus.hpp
+++ Fibonacci/FibonacciCplusplus.hpp
@@ -7,6 +7,9 @@ The C++ API.
 
 #pragma once
 
+#include <Fibonacci/Fibonacci-Swift.h>
+#include <sstream>
+
 class FibonacciCalculatorCplusplus {
 public:
     FibonacciCalculatorCplusplus(bool printInvocation);
@@ -14,3 +17,9 @@ public:
 private:
     bool printInvocation;
 };
+
+inline swift::String makeSwiftStringFromChar(char whatever) {
+    std::ostringstream os;
+    os << "The char is " << whatever << "." << std::endl;
+    return swift::String(os.str());
+}

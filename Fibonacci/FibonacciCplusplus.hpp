296945d2fbbabf731edbf1c3fe4e530245dce537
Adrian Taylor <adrian_taylor@apple.com>
2025-10-10 12:59:05 +0100

    min

--- Fibonacci/FibonacciCplusplus.hpp
+++ Fibonacci/FibonacciCplusplus.hpp
@@ -52,10 +52,6 @@ public:
     Function() = default;
     Function(std::nullptr_t) { }
 
-    template<typename CallableType, class = typename std::enable_if<!(std::is_pointer<CallableType>::value && std::is_function<typename std::remove_pointer<CallableType>::type>::value) && std::is_rvalue_reference<CallableType&&>::value>::type>
-    Function(CallableType&& callable)
-        : m_callableWrapper(makeUnique<CallableWrapper<CallableType, Out, In...>>(std::forward<CallableType>(callable))) { }
-
     template<typename FunctionType, class = typename std::enable_if<std::is_pointer<FunctionType>::value && std::is_function<typename std::remove_pointer<FunctionType>::type>::value>::type>
     Function(FunctionType f)
         : m_callableWrapper(makeUnique<CallableWrapper<FunctionType, Out, In...>>(std::forward<FunctionType>(f))) { }
@@ -66,15 +62,6 @@ public:
         return m_callableWrapper->call(std::forward<In>(in)...);
     }
 
-    explicit operator bool() const { return !!m_callableWrapper; }
-
-    template<typename CallableType, class = typename std::enable_if<!(std::is_pointer<CallableType>::value && std::is_function<typename std::remove_pointer<CallableType>::type>::value) && std::is_rvalue_reference<CallableType&&>::value>::type>
-    Function& operator=(CallableType&& callable)
-    {
-        m_callableWrapper = makeUnique<CallableWrapper<CallableType, Out, In...>>(std::forward<CallableType>(callable));
-        return *this;
-    }
-
     template<typename FunctionType, class = typename std::enable_if<std::is_pointer<FunctionType>::value && std::is_function<typename std::remove_pointer<FunctionType>::type>::value>::type>
     Function& operator=(FunctionType f)
     {
@@ -82,16 +69,6 @@ public:
         return *this;
     }
 
-    Function& operator=(std::nullptr_t)
-    {
-        m_callableWrapper = nullptr;
-        return *this;
-    }
-
-    Impl* leak()
-    {
-        return m_callableWrapper.release();
-    }
 
 private:
     enum AdoptTag { Adopt };
@@ -100,15 +77,13 @@ private:
     {
     }
 
-    friend Function adopt<Out, In...>(Impl*);
-
     std::unique_ptr<Impl> m_callableWrapper;
 };
 
-template<typename Out, typename... In> Function<Out(In...)> adopt(CallableWrapperBase<Out, In...>* impl)
-{
-    return Function<Out(In...)>(impl, Function<Out(In...)>::Adopt);
-}
+//template<typename Out, typename... In> Function<Out(In...)> adopt(CallableWrapperBase<Out, In...>* impl)
+//{
+//    return Function<Out(In...)>(impl, Function<Out(In...)>::Adopt);
+//}
 
 struct SomeInput {
     int value;

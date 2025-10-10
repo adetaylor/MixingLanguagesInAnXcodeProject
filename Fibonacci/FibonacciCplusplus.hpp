291d7781059f5455e5bc474b479d4c163babbd41
Adrian Taylor <adrian_taylor@apple.com>
2025-10-10 13:02:03 +0100

    min

--- Fibonacci/FibonacciCplusplus.hpp
+++ Fibonacci/FibonacciCplusplus.hpp
@@ -18,61 +18,32 @@ private:
 };
 
 
-template<typename Out, typename... In>
-class CallableWrapperBase {
-public:
-    virtual ~CallableWrapperBase() { }
-    virtual Out call(In...) = 0;
-};
-
 template<typename, typename, typename...> class CallableWrapper;
 
 template<typename CallableType, typename Out, typename... In>
-class CallableWrapper : public CallableWrapperBase<Out, In...> {
+class CallableWrapper {
 public:
     explicit CallableWrapper(CallableType&& callable)
         : m_callable(WTFMove(callable)) { }
     CallableWrapper(const CallableWrapper&) = delete;
-    CallableWrapper& operator=(const CallableWrapper&) = delete;
-    Out call(In... in) final { return m_callable(std::forward<In>(in)...); }
+    virtual ~CallableWrapper() { }
 private:
     CallableType m_callable;
 };
 
-
 template<typename> class Function;
-
-template<typename Out, typename... In> Function<Out(In...)> adopt(CallableWrapperBase<Out, In...>*);
 
 template <typename Out, typename... In>
 class Function<Out(In...)> {
 public:
-    using Impl = CallableWrapperBase<Out, In...>;
-
-    Function() = default;
-    Function(std::nullptr_t) { }
+    using Impl = CallableWrapper<Out, In...>;
 
     template<typename FunctionType, class = typename std::enable_if<std::is_pointer<FunctionType>::value && std::is_function<typename std::remove_pointer<FunctionType>::type>::value>::type>
     Function(FunctionType f)
         : m_callableWrapper(makeUnique<CallableWrapper<FunctionType, Out, In...>>(std::forward<FunctionType>(f))) { }
 
-    Out operator()(In... in) const
-    {
-        ASSERT(m_callableWrapper);
-        return m_callableWrapper->call(std::forward<In>(in)...);
-    }
-
-    template<typename FunctionType, class = typename std::enable_if<std::is_pointer<FunctionType>::value && std::is_function<typename std::remove_pointer<FunctionType>::type>::value>::type>
-    Function& operator=(FunctionType f)
-    {
-        m_callableWrapper = makeUnique<CallableWrapper<FunctionType, Out, In...>>(std::forward<FunctionType>(f));
-        return *this;
-    }
-
-
 private:
-    enum AdoptTag { Adopt };
-    Function(Impl* impl, AdoptTag)
+    Function(Impl* impl)
         : m_callableWrapper(impl)
     {
     }
@@ -80,13 +51,12 @@ private:
     std::unique_ptr<Impl> m_callableWrapper;
 };
 
-//template<typename Out, typename... In> Function<Out(In...)> adopt(CallableWrapperBase<Out, In...>* impl)
-//{
-//    return Function<Out(In...)>(impl, Function<Out(In...)>::Adopt);
-//}
-
 struct SomeInput {
     int value;
 };
 
 using InputTest = Function<bool (SomeInput&)>;
+
+inline void callFunctionyThing(InputTest&& test) {
+    
+}

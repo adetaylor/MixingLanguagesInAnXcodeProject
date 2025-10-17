c322905efb1ceb21e8a997e0916fcb0c49154e0e
Adrian Taylor <adrian_taylor@apple.com>
2025-10-17 15:08:27 +0100

    Demonstrate storing Swift thing as C++ member

--- Fibonacci/FibonacciCplusplus.cpp
+++ Fibonacci/FibonacciCplusplus.cpp
@@ -9,18 +9,14 @@ Implements the C++ API.
 #include <Fibonacci/Fibonacci-Swift.h>
 #include <iostream>
 
-FibonacciCalculatorCplusplus::FibonacciCalculatorCplusplus(bool printInvocation) : printInvocation(printInvocation) {}
+FibonacciCalculatorCplusplus::FibonacciCalculatorCplusplus(bool printInvocation) : m_swiftCalculator(std::make_unique<Fibonacci::FibonacciCalculator>(Fibonacci::FibonacciCalculator::init(printInvocation))) {}
 
 double FibonacciCalculatorCplusplus::fibonacci(double value) const {
-    // Print the value if applicable.
-    if (printInvocation)
-        std::cout << "[c++] fibonacci(" << value << ")\n";
-    
     // Handle the base case of the recursion.
     if (value <= 1.0)
         return 1.0;
-    
-    // Create the Swift `FibonacciCalculator` structure and invoke its `fibonacci` method.
-    auto swiftCalculator = Fibonacci::FibonacciCalculator::init(printInvocation);
-    return swiftCalculator.fibonacci(value - 1.0) + swiftCalculator.fibonacci(value - 2.0);
+
+    return m_swiftCalculator->fibonacci(value - 1.0) + m_swiftCalculator->fibonacci(value - 2.0);
 }
+
+FibonacciCalculatorCplusplus::~FibonacciCalculatorCplusplus() {}
--- Fibonacci/FibonacciCplusplus.hpp
+++ Fibonacci/FibonacciCplusplus.hpp
@@ -6,11 +6,21 @@ The C++ API.
 */
 
 #pragma once
+#include <memory>
+#include <swift/bridging>
+
+// #include <Fibonacci/Fibonacci-Swift.h> // not OK
+
+namespace Fibonacci {
+class FibonacciCalculator;
+}
 
 class FibonacciCalculatorCplusplus {
 public:
     FibonacciCalculatorCplusplus(bool printInvocation);
+    FibonacciCalculatorCplusplus(FibonacciCalculatorCplusplus&&) = default;
     double fibonacci(double value) const;
+    ~FibonacciCalculatorCplusplus();
 private:
-    bool printInvocation;
-};
+    std::unique_ptr<Fibonacci::FibonacciCalculator> m_swiftCalculator;
+} SWIFT_ESCAPABLE;
